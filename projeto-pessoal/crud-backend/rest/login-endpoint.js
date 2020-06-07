3acf5c886319c9aae61536e7e4a042b86cdd2b63
joseivoknery <joseivokoerichnery@gmail.com>
2020-06-06 21:51:32 -0300

    V3

--- projeto-pessoal/crud-backend/rest/login-endpoint.js
+++ projeto-pessoal/crud-backend/rest/login-endpoint.js
@@ -8,4 +8,9 @@ router.post('/', async (req, res) => {
   res.status(response.status).json(response);
 })
 
+router.get('/', async (req, res) => {
+  const response = await service.logout(req.body);
+  res.status(response.status).json(response);
+});
+
 module.exports = router;
--- projeto-pessoal/crud-backend/service/login-service.js
+++ projeto-pessoal/crud-backend/service/login-service.js
@@ -8,52 +8,65 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const app = require('../server');
 
+const logout = async () => {
+  response.body = security(utils.STRING_EMPTY);
+  response.mensagem = 'Logout realizado com Sucesso!';
+  response.status = httpStatus.STATUS_OK;
+
+  return response;
+};
+
 const login = async (body) => {
+  let usuario = await Usuario.findOne({ user: body.user });
 
-    let usuario = await Usuario.findOne({user: body.user});
+  if (!usuario) {
+    response.body = security(utils.STRING_EMPTY);
+    response.mensagem =
+      "'Não foi encontrado um usuário com o login informado!'";
+    response.status = httpStatus.STATUS_Forbidden;
+  } else if (await bcrypt.compare(body.senha, usuario.senha)) {
+    let payload = gerarPayload(usuario);
+    let token = gerarToken(payload, process.env.SECRET);
+    //Adicionar o acesso aos Endpoints privados aqui
+    response.body = security(token);
+    response.mensagem = 'Acesso Permitido';
+    response.status = httpStatus.STATUS_OK;
+  } else {
+    response.body = security(utils.STRING_EMPTY);
+    response.mensagem = 'Senha inválida!';
+    response.status = httpStatus.STATUS_Forbidden;
+  }
 
-    if (!usuario) {
-      response.body = security(utils.STRING_EMPTY);
-      response.mensagem = "'Não foi encontrado um usuário com o login informado!'";
-      response.status = httpStatus.STATUS_Forbidden;
-    } 
-    else if (await bcrypt.compare(body.senha, usuario.senha)) {
-      let payload = gerarPayload(usuario);
-      let token = gerarToken(payload, process.env.SECRET);
-      //Adicionar o acesso aos Endpoints privados aqui
-      response.body = security(token);
-      response.mensagem = "Acesso Permitido";
-      response.status = httpStatus.STATUS_OK;
-    } 
-    else {
-      response.body = security(utils.STRING_EMPTY);
-      response.mensagem = "Senha inválida!";
-      response.status = httpStatus.STATUS_Forbidden;
-    }
+  return response;
+};
 
-    return response;
-}
-
-const gerarToken = (payload, secret) =>{
+const gerarToken = (payload, secret) => {
   return jwt.sign(payload, secret, {
-    expiresIn: utils.TEMPO_TOKEN_EXPIRE // expira em 5 minutos (300 segundos)
+    expiresIn: utils.TEMPO_TOKEN_EXPIRE, // expira em 5 minutos (300 segundos)
   });
 };
 
-const gerarPayload = (load) =>{
-  Payload.id =  load._id;
+const validaToken = (token) => {
+  return token === utils.STRING_EMPTY || token === null || token === undefined
+    ? null
+    : token;
+};
+
+const gerarPayload = (load) => {
+  Payload.id = load._id;
   Payload.login = load.user;
   return Payload;
 };
 
-const security = (token) =>{
-  Security.auth = token === utils.STRING_EMPTY ? false : true;
-  Security.token = token;
+const security = (token) => {
+  Security.auth = validaToken(token) === null ? false : true;
+  Security.token = validaToken(token);
   return Security;
 };
 
 const methods = {
-  login: login
+  login: login,
+  logout: logout
 };
 
 module.exports = methods;
