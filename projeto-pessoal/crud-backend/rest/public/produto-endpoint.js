3df7cd16bc8aab1c4d567fb9c563800a9dea5bc0
joseivoknery <joseivokoerichnery@gmail.com>
2020-06-07 16:06:37 -0300

    V3

--- projeto-pessoal/crud-backend/rest/public/produto-endpoint.js
+++ projeto-pessoal/crud-backend/rest/public/produto-endpoint.js
@@ -2,30 +2,6 @@ const express = require('express');
 const router = express.Router();
 const service = require('../service/produto-service');
 
-// cria um produto
-router.post('/', async (req, res) => {
-    const response = await service.salvarProduto(req.body);
-    res.status(response.status).json(response);
-})
-
-// atualiza um produto pelo id
-router.patch('/:id', async (req, res) => {
-    const response = await service.editarProduto(req.params.id, req.body);
-    res.status(response.status).json(response);
-})
-
-// inicializa a massa de teste
-router.get('/inicializa', async (req, res) => {
-    const response = await service.inicializa();
-    res.status(response.status).send(response);
-})
-
-// remove um produto
-router.delete('/:id', async (req, res) => {
-    let response = await service.removerProduto(req.params.id);
-    res.status(response.status).json(response);
-})
-
 // retorna todos os produtos
 router.get('/', async (req, res) => {
     let response = await service.listarTodos();
