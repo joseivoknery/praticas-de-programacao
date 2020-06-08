4cf638ede3b9424148f6311a77bb7edf5ef7f5c1
joseivoknery <joseivokoerichnery@gmail.com>
2020-06-08 00:19:07 -0300

    V4

--- projeto-pessoal/crud-backend/rest/private/login-endpoint.js
+++ projeto-pessoal/crud-backend/rest/private/login-endpoint.js
@@ -3,30 +3,10 @@ const router = express.Router();
 const service = require('../../service/login-service');
 const nivelAcesso = require('../../utils/nivel');
 
-
 // realizar cadastro de um novo usuário/cliente no sistema
 router.post('/signup', async (req, res) => {
   let response = await service.singUp(req.body, nivelAcesso.ADMIN);
   res.status(response.status).json(response);
 })
-
-// realizar login no sistema
-router.post('/', async (req, res) => {
-  let response = await service.login(req.body);
-  res.status(response.status).json(response);
-})
-
-// realizar logout no sistema
-router.get('/', async (req, res) => {
-  let response = await service.logout();
-  res.status(response.status).json(response);
-});
-
-// verifica se o token é valido
-router.get('/valid', service.validarToken, (req, res, next) => {
-  res.status(req.body.status);
-});
-
-
 
 module.exports = router;
