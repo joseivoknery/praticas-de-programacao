44930f9f98af27e8cc0a997eae10b3a2f87af58a
jose.nery <jose.nery@ctis.com.br>
2020-06-22 15:18:56 -0300

    V5

--- projeto-pessoal/crud-backend/environment.js
+++ projeto-pessoal/crud-backend/environment.js
@@ -1,7 +1,7 @@
 const DATABASE_URL = "mongodb://localhost/crud-backend";
 const SERVER_PORT = 5000;
-const SECRET = "jiknVSC";
-const SALT_ROUNDS = 12;
+const SECRET = "koerichNery";
+const SALT_ROUNDS = 20;
 
 const environment = {
   DATABASE_URL: DATABASE_URL,
